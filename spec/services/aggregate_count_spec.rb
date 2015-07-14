18b950f004fbd0783ffb369e2ed68a23cce4bbc1
Hong ChulJu <ang0123dev@gmail.com>
2015-07-15 01:54:29 +0900

    Add a Services::AggregateCount spec

--- spec/services/aggregate_count_spec.rb
+++ spec/services/aggregate_count_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Services::AggregateCount do
+  let(:project_id) { 'chocolat' }
+  let(:event_name) { 'commits' }
 
+  let(:connection) { Repository::Database.connection(database: project_id) }
+  let!(:database) { connection.database }
+  let!(:collection) { database.collection(event_name) }
+
+  after :each do
+    database.drop
+  end
+
+  it 'counts just the number of events for no given condition' do
+    result = Services::AggregateCount.new(project_id, event_name).execute
+    expect(result.success?).to be true
+    expect(result.value).to eq(0)
+
+    collection.insert(v: 1)
+    result = Services::AggregateCount.new(project_id, event_name).execute
+    expect(result.success?).to be true
+    expect(result.value).to eq(1)
+  end
 end
